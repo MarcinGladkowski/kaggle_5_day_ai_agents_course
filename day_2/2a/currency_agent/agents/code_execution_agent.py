b7d8a2b1c1dfb2043091d7f62a7f38e181e977ee
MarcinGladkowski <marcingladkowski@gmail.com>
2025-11-17 06:38:21 +0100

    day_2: 2a - currency agent add code execution agent calculation step

---
+++ day_2/2a/currency_agent/agents/code_execution_agent.py
@@ -0,0 +1,22 @@
+from google.adk.agents.llm_agent import LlmAgent
+from google.adk.code_executors import BuiltInCodeExecutor # Key import
+
+
+calculation_agent = LlmAgent(
+    name="CalculationAgent",
+    model="gemini-2.5-flash-lite",
+    instruction="""You are a specialized calculator that ONLY responds with Python code. You are forbidden from providing any text, explanations, or conversational responses.
+ 
+     Your task is to take a request for a calculation and translate it into a single block of Python code that calculates the answer.
+     
+     **RULES:**
+    1.  Your output MUST be ONLY a Python code block.
+    2.  Do NOT write any text before or after the code block.
+    3.  The Python code MUST calculate the result.
+    4.  The Python code MUST print the final result to stdout.
+    5.  You are PROHIBITED from performing the calculation yourself. Your only job is to generate the code that will perform the calculation.
+   
+    Failure to follow these rules will result in an error.
+       """,
+    code_executor=BuiltInCodeExecutor(),  # Use the built-in Code Executor Tool. This gives the agent code execution capabilities
+)
